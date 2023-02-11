2f02d5a8dc35595aef99fcf63508a3d88269d02e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-11 18:41:38 +0800

    Set A4 to receive children elements

--- src/components/A4.tsx
+++ src/components/A4.tsx
@@ -1,9 +1,15 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class A4 extends Component {
+interface Props {
+    children?: React.ReactNode;
+}
+
+class A4 extends Component<Props> {
     render() {
         return (
-            <div className='A4 bg-gray-100'></div>
+            <div className='A4 bg-gray-100 '>
+                {this.props.children}
+            </div>
         )
     }
 }
