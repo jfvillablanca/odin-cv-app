03f5cde773d55c0e468e60098fcf983780b5b0a9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-11 18:09:05 +0800

    Add A4.tsx

---
+++ src/components/A4.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class A4 extends Component {
+    render() {
+        return (
+            <div className='A4 bg-gray-100'></div>
+        )
+    }
+}
+
+export default A4;
