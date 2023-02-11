2e0adec27c09497088785c61758b9147eef2b497
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-11 08:11:16 +0800

    Convert project to typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className='App'>
+            <h1>I should be able to run in TypeScript</h1>
+        </div>
+    );
+}
+
+export default App;
