afa86a4725e1f99c6d891fbe9477face64e42d40
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-11 18:10:16 +0800

    Import A4 to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Component } from "react";
 import "./App.scss";
+import A4 from "./components/A4";
 
 class App extends Component {
     render() {
         return (
-            <div className='App h-screen w-screen bg-gray-500'>
+            <div className='App bg-gray-500 py-28'>
+                <A4 />
             </div>
         );
     }
