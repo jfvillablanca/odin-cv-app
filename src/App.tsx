6b1a2bb08a0f2d5bd5b6c8470db767ecd8cbbda3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-13 12:05:00 +0800

    Add func: headerTemplate prop to Header

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,29 @@ interface Field {
     p?: string;
 }
 
+interface HeaderValues {
+    fullName: string;
+    statement?: string;
+    subfields?: Array<[string, string, string]>;
+}
+
 class App extends Component {
+    headerTemplate: HeaderValues = {
+        fullName: "Lorem Ipsum",
+        statement:
+            "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi.",
+        subfields: [
+            [nanoid(), "profession", "professor"],
+            [nanoid(), "phone", "696969"],
+            [nanoid(), "email", "email@email.com"],
+        ],
+    };
+
     render() {
         return (
             <div className='App bg-gray-500 py-14 px-10'>
                 <A4>
-                    <Header />
+                    <Header headerValues={this.headerTemplate} />
                     <BlockContainer>
                     </BlockContainer>
                 </A4>
