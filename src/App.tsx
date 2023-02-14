88db5824170b983f991bd2a4dfb2ba352a92468b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-14 18:58:59 +0800

    Add func: addtl optional data attribs to parse

--- src/App.tsx
+++ src/App.tsx
@@ -69,8 +69,11 @@ class App extends Component<{}, State> {
         const target = event.target as HTMLInputElement;
         const stateKeys = (target.dataset as any).name.split("|") as [
             string,
-            string
+            string,
+            string?,
+            string?
         ];
+        const [key1, key2, key3 = '', key4 = ''] = stateKeys;
 
         this.setState((prevState) => {
             const updatedState = { ...prevState };
