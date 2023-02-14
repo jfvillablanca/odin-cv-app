58dcd275101dff0bcc2fccd25af9ea19d69e50aa
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 07:52:23 +0800

    Add minimal style to HoverButton

--- src/components/HoverButton.tsx
+++ src/components/HoverButton.tsx
@@ -1,6 +1,14 @@
 import { Component } from "react";
 
 class HoverButton extends Component {
+    render() {
+        const field = 'field';
+        return(
+            <div className="absolute h-12 bottom-0 translate-y-12 -left-36 ">
+                <button className="bg-zinc-50 border-gray-500 text-gray-500 opacity-80 text-lg border-2 rounded-2xl py-1 px-4 uppercase">{`New ${field}`}</button>
+            </div>
+        )
+    }
 }
 
 export default HoverButton
