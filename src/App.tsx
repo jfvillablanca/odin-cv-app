42d6af4f460bab896f8a3354bea62dd064a949a5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-14 08:31:15 +0800

    Add func: handleFormInput

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,17 @@ class App extends Component<{}, State> {
         ] as Array<Field>,
     };
 
+    handleFormInput = (event: React.SyntheticEvent) => {
+        const target = event.target as HTMLInputElement;
+        const stateKeys = (target.dataset as any).name.split("|") as [string, string];
+
+        this.setState(prevState => {
+            const updatedState = {...prevState};
+            updatedState[stateKeys[0]][stateKeys[1]] = target.value;
+            return updatedState;
+        })
+    }
+
     handleEditMode = () => {
         this.setState(prevState => ({ editMode: !prevState.editMode }))
     }
