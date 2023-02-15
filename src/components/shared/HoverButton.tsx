a0609cad67b1e28085ed64f0cd5c05b8d3fbb461
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 13:23:28 +0800

    Update hoverButton textContent

--- src/components/shared/HoverButton.tsx
+++ src/components/shared/HoverButton.tsx
@@ -6,7 +6,7 @@ class HoverButton extends Component {
         return(
             <div className="absolute z-10 h-12 bottom-0 transform -translate-x-1/2 translate-y-12 left-1/2">
                 <div className="border-b-2 border-gray-500"></div>
-                <button className="bg-zinc-50 border-gray-500 text-gray-500 opacity-80 text-lg border-2 rounded-2xl py-1 px-4 uppercase">{`New ${field}`}</button>
+                <button className="bg-zinc-50 border-gray-500 text-gray-500 opacity-80 text-lg border-2 rounded-2xl py-1 px-4 uppercase">{`Insert ${field} below`}</button>
             </div>
         )
     }
