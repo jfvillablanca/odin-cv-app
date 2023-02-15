475394f86a7597fcc35abc52b208153c48d80e5c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 12:18:19 +0800

    Dynamic button bgcolor and add shadow on hover

--- src/components/ModifyButton.tsx
+++ src/components/ModifyButton.tsx
@@ -11,13 +11,19 @@ class ModifyButton extends Component<Props> {
             this.props.documentMode === "field"
                 ? "Add/Delete Sections"
                 : "Edit Fields";
+
+        const buttonBgColor =
+            this.props.documentMode === "field"
+                ? "bg-orange-700"
+                : "bg-teal-600";
+
         const toggleDocumentMode = this.props.toggleDocumentMode;
 
         return (
             <div className='fixed z-10 h-12 top-0 transform -translate-x-1/2 left-1/2 mt-16'>
                 <button
                     onClick={toggleDocumentMode}
-                    className='bg-orange-700 text-zinc-50 opacity-80 text-lg rounded-2xl py-3 px-4 uppercase'
+                    className={`${buttonBgColor} text-zinc-50 opacity-80 text-lg rounded-2xl py-3 px-4 uppercase hover:shadow-gray-400 hover:shadow-md`}
                 >
                     {mode}
                 </button>
