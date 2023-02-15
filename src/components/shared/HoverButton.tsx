549c50fdcd68af76c1769f2db872855231dda78a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 18:04:42 +0800

    Pass dataName back to App from HoverButton Delete

--- src/components/shared/HoverButton.tsx
+++ src/components/shared/HoverButton.tsx
@@ -3,18 +3,22 @@ import { Component } from "react";
 interface Props {
     canFieldBeRemoved?: boolean;
     handleOnClickDeleteField?: Function;
+    dataName?: string;
 }
 
 class HoverButton extends Component<Props> {
     render() {
         const handleOnClickDeleteField = this.props
-            .handleOnClickDeleteField as (event: React.SyntheticEvent) => void;
-        const { canFieldBeRemoved = true } = this.props;
+            .handleOnClickDeleteField as (dataName: string) => void;
+        const { dataName, canFieldBeRemoved = true } = this.props;
         return (
             <div className='absolute flex gap-2 z-10 h-12 bottom-0 transform -translate-x-1/2 translate-y-12 left-1/2'>
                 <InsertButton />
                 {canFieldBeRemoved && (
-                    <DeleteButton handleClick={handleOnClickDeleteField} />
+                    <DeleteButton
+                        field={dataName as string}
+                        handleClick={handleOnClickDeleteField}
+                    />
                 )}
             </div>
         );
@@ -30,15 +34,17 @@ class InsertButton extends Component {
 }
 
 class DeleteButton extends Component<{
-    handleClick: (event: React.SyntheticEvent) => void;
+    field: string;
+    handleClick: (dataName: string) => void;
 }> {
     render() {
         const handleClick = this.props.handleClick as (
-            event: React.SyntheticEvent
+            dataName: string
         ) => void;
+        const field = this.props.field;
         return (
             <button
-                onClick={(event) => handleClick(event)}
+                onClick={() => handleClick(field)}
                 className='bg-zinc-50 border-2 border-red-500 text-red-500 hover:bg-red-500 hover:text-zinc-50 hover:border-red-500 opacity-80 text-lg rounded-2xl py-1 px-4 uppercase'
             >{`Delete field`}</button>
         );
