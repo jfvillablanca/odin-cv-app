e58742e484e8ba2ecf81243ad6a4b960e7eb9e83
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 11:30:30 +0800

    Add feat: textColor toggle for documentMode

--- src/components/A4.tsx
+++ src/components/A4.tsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 interface Props {
+    documentMode: "section" | "field";
     children?: React.ReactNode;
 }
 
 class A4 extends Component<Props> {
     render() {
+        const textColor = 
+            this.props.documentMode === "field"
+                ? "text-gray-800" 
+                : "text-gray-400";
+
         return (
             <div className='A4 relative bg-gray-100 text-gray-800'>
                 {this.props.children}
