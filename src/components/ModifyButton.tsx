b03de0492f3308de7d8e6d1aae363e5b96ef5f54
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-15 11:08:38 +0800

    Chore: remove unnecessary syntax lol

--- src/components/ModifyButton.tsx
+++ src/components/ModifyButton.tsx
@@ -5,7 +5,7 @@ class ModifyButton extends Component {
         const mode = true ? 'Add/Delete Sections' : 'Edit Fields';
         return(
             <div className="fixed z-10 h-12 top-0 transform -translate-x-1/2 left-1/2 mt-16">
-                <button className="bg-orange-700 text-zinc-50 opacity-80 text-lg rounded-2xl py-3 px-4 uppercase">{`${mode}`}</button>
+                <button className="bg-orange-700 text-zinc-50 opacity-80 text-lg rounded-2xl py-3 px-4 uppercase">{mode}</button>
             </div>
         )
     }
