1cf7d41fc57f7b033bf6dd1b6e68eec1ca8267ab
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-16 12:29:20 +0800

    Refactor: switch FullName to FC for useContext hook

--- src/components/Header/FullName.tsx
+++ src/components/Header/FullName.tsx
@@ -1,66 +1,43 @@
-import { Component } from "react";
+import { FC, useContext, useState } from "react";
+import { AppContext } from "../shared/AppContext";
 import Field from "../shared/Field";
 
 interface Props {
     hoverColor: string;
-    isEditMode: boolean;
-    handleOnClick: Function;
-    handleOnBlur: Function;
-    handleFormInput: Function;
-    activeField: string | null;
+    dataName: string;
     fullName: string;
     children?: React.ReactNode;
 }
 
-class FullName extends Component<Props> {
-    state = {
-        isHovered: false,
+const FullName: FC<Props> = ({ hoverColor, dataName, fullName, children }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(() => true);
     };
 
-    handleMouseEnter = () => {
-        this.setState({ isHovered: true });
+    const handleMouseLeave = () => {
+        setIsHovered(() => false);
     };
 
-    handleMouseLeave = () => {
-        this.setState({ isHovered: false });
-    };
+    const { editMode } = useContext(AppContext).state;
 
-    render() {
-        const hoverButton = this.props.children;
-        const hoverColor = this.props.hoverColor;
-        const isEditMode = this.props.isEditMode;
-        const activeField = this.props.activeField;
-        const fullName = this.props.fullName;
-        const handleOnClick = this.props.handleOnClick as (
-            dataName: string
-        ) => void;
-        const handleOnBlur = this.props.handleOnBlur as (
-            dataName: string
-        ) => void;
-        const handleFormInput = this.props.handleFormInput as (
-            event: React.SyntheticEvent
-        ) => void;
-        return (
-            <div
-                className={!isEditMode ? `${hoverColor} relative` : "relative"}
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-            >
-                <Field
-                    tag={"h1"}
-                    isEditMode={isEditMode}
-                    className='w-full font-semibold text-3xl mb-6'
-                    dataName={"headerFields|fullName"}
-                    textContent={fullName}
-                    handleOnClick={handleOnClick}
-                    handleOnBlur={handleOnBlur}
-                    handleFormInput={handleFormInput}
-                    activeField={activeField}
-                />
-                {this.state.isHovered && hoverButton}
-            </div>
-        );
-    }
-}
+    const hoverButton = children;
+    return (
+        <div
+            className={!editMode ? `${hoverColor} relative` : "relative"}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <Field
+                tag={"h1"}
+                className='w-full font-semibold text-3xl mb-6'
+                dataName={dataName}
+                textContent={fullName}
+            />
+            {isHovered && hoverButton}
+        </div>
+    );
+};
 
 export default FullName;
