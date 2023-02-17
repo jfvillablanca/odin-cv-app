d02413d0e6b5ec6a985e2fee908e99ae96aaa88a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-17 19:27:39 +0800

    Update AppContext to use fieldId

--- src/components/shared/AppContext.tsx
+++ src/components/shared/AppContext.tsx
@@ -3,17 +3,17 @@ import { State } from "../../App";
 
 export type AppContextType = {
     state: State;
-    handleFormInput: (event: React.SyntheticEvent) => void;
-    handleOnClickFormField: (dataName: string) => void;
-    handleOnBlurFormField: (dataName: string) => void;
-    handleOnClickDeleteField: (dataName: string) => void;
-    handleOnClickInsertField: (dataName: string) => void;
+    handleFormInput: (event: React.SyntheticEvent, fieldId: string) => void;
+    handleOnClickFormField: (fieldId: string) => void;
+    handleOnBlurFormField: (fieldId: string) => void;
+    handleOnClickDeleteField: (fieldId: string) => void;
+    handleOnClickInsertField: (fieldId: string) => void;
     toggleDocumentMode: () => void;
 }
 
 export const AppContext = createContext({
     state: {},
-    handleFormInput: (_: React.SyntheticEvent) => void 0,
+    handleFormInput: (_: React.SyntheticEvent, __: string) => void 0,
     handleOnClickFormField: (_: string) => void 0,
     handleOnBlurFormField: (_: string) => void 0,
     handleOnClickDeleteField: (_: string) => void 0,
@@ -30,10 +30,10 @@ export const AppContext = createContext({
 export const createAppContextValue = (
     state: State, 
     handleFormInput: (event: React.SyntheticEvent) => void,
-    handleOnClickFormField: (dataName: string) => void,
-    handleOnBlurFormField: (dataName: string) => void,
-    handleOnClickDeleteField: (dataName: string) => void,
-    handleOnClickInsertField: (dataName: string) => void,
+    handleOnClickFormField: (fieldId: string) => void,
+    handleOnBlurFormField: (fieldId: string) => void,
+    handleOnClickDeleteField: (fieldId: string) => void,
+    handleOnClickInsertField: (fieldId: string) => void,
     toggleDocumentMode: () => void,
 ): AppContextType => ({
   state,
