26efa4f09999fa4f7dc8f32600ea4bb569ff8f6d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-17 14:45:41 +0800

    Chore: cleanup unused placeholder text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { Component } from "react";
-import HoverButton from "../shared/HoverButton";
 import { OrderedFieldsToRender } from "../../App";
 import FieldWrapper from "./FieldWrapper";
 import HeaderSubfields from "./HeaderSubfields";
@@ -20,40 +19,10 @@ const hoverColor = {
 
 class Header extends Component<Props> {
     render() {
-        const documentMode = this.props.documentMode;
-        const { fullName, statement, subfields = [] } = this.props.headerFields;
 
         return (
             <div className='Section grid w-full mb-9'>
                 <div className='flex flex-col'>
-                    <FieldWrapper
-                        tag={"h1"}
-                        tailwindStyles={"w-full font-semibold text-3xl mb-6"}
-                        hoverColor={hoverColor.blue}
-                        dataName={"headerFields|fullName"}
-                        textContent={fullName}
-                    >
-                        {documentMode === "section" && (
-                            <HoverButton canFieldBeRemoved={false} />
-                        )}
-                    </FieldWrapper>
-                    {!!statement && (
-                            hoverColor={hoverColor.blue}
-                        >
-                    )}
-                    {subfields.length !== 0 && (
-                        <HeaderSubfields
-                            hoverColor={hoverColor.blue}
-                            isEditMode={isEditMode}
-                            handleOnClick={handleOnClickFormField}
-                            handleOnBlur={handleOnBlurFormField}
-                            handleFormInput={handleFormInput}
-                            activeField={activeField}
-                            subfields={subfields}
-                        >
-                            {documentMode === "section" && <HoverButton />}
-                        </HeaderSubfields>
-                    )}
                 </div>
             </div>
         );
