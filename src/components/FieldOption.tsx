e4baaa7ec3cfb7a394e6e6540dd0d295a6d3d52b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-19 14:04:41 +0800

    Import AppContext and useContext

--- src/components/FieldOption.tsx
+++ src/components/FieldOption.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { AppContext } from "./shared/AppContext";
+
 function FieldOptionContainer() {
     const tailwindStyles =
         "fixed z-10 top-0 left-11 mt-16 flex flex-col gap-2 bg-gray-100 py-4 px-4 rounded-lg";
