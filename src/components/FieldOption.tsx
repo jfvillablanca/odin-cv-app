9fa7a4434e52850a2a2cf0eb55c44c02299c4252
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-19 13:16:04 +0800

    Style: position insert options

--- src/components/FieldOption.tsx
+++ src/components/FieldOption.tsx
@@ -1,4 +1,11 @@
 function FieldOptionContainer() {
+    const tailwindStyles =
+        "fixed z-10 top-0 left-11 mt-16 flex flex-col gap-2 bg-gray-100 py-4 px-4 rounded-lg";
+
+    return (
+        <div className={tailwindStyles}>
+        </div>
+    );
 }
 
 export default FieldOptionContainer;
